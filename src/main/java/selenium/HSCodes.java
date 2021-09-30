a9bf225250999e6c456261a4799cf4864f91c1b3
edwincmj <80372170+edwincmj@users.noreply.github.com>
2021-09-30 22:45:34 +0800

    Create HSCodes.java
    arraylist to create HSCodes for individual product group for Indonesia scrapper

---
+++ src/main/java/selenium/HSCodes.java
@@ -0,0 +1,48 @@
+package selenium;
+import java.util.*;
+
+public class HSCodes {
+    public static ArrayList<ArrayList<Integer>> getList(){
+        ArrayList<ArrayList<Integer>> hello = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(27090010);
+        temp.add(27090090);
+        hello.add(temp);
+        ArrayList<Integer> temp2 = new ArrayList<>();
+        temp2.add(27090020);
+        hello.add(temp2);
+        ArrayList<Integer> temp3 = new ArrayList<>();
+        temp3.add(27101211);
+        temp3.add(27101212);
+        temp3.add(27101213);
+        temp3.add(27101221);
+        temp3.add(27101222);
+        temp3.add(27101223);
+        temp3.add(27101224);
+        temp3.add(27101225);
+        temp3.add(27101226);
+        temp3.add(27101227);
+        temp3.add(27101228);
+        temp3.add(27101229);
+        hello.add(temp3);
+        ArrayList<Integer> temp4 = new ArrayList<>();
+        temp4.add(27101270);
+        temp4.add(27101280);
+        hello.add(temp4);
+        ArrayList<Integer> temp5 = new ArrayList<>();
+        temp5.add(27101981);
+        temp5.add(27101982);
+        temp5.add(27101983);
+        hello.add(temp5);
+        ArrayList<Integer> temp6 = new ArrayList<>();
+        temp6.add(27101971);
+        temp6.add(27101972);
+        hello.add(temp6);
+        ArrayList<Integer> temp7 = new ArrayList<>();
+        temp7.add(27101979);
+        hello.add(temp7);
+
+        return hello;
+
+    }
+}
