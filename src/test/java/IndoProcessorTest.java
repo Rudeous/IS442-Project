4c32e840e8485478c2ff341c9a86eef01e639c1e
edwincmj <80372170+edwincmj@users.noreply.github.com>
2021-10-10 16:54:12 +0800

    Create test classes

---
+++ src/test/java/IndoProcessorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import processor.IndoProcessor;
+import selenium.HSCodes;
+
+public class IndoProcessorTest {
+    @Test
+    void Test() throws InterruptedException {
+        IndoProcessor.process();
+    }
+}
