bff74ab69d83c4665299a89445cd3c5391e2d8c5
linghui <lingxhui@gmail.com>
2021-10-19 15:54:37 +0800

    added MongoDbTest.java

---
+++ src/test/java/MongoDbTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class MongoDbTest {
+    @Test
+    void Test() {
+        MongoDbProvider provider1 = new MongoDbProvider("mongodb:localhost:27017");
+        MongoDbProvider provider2 = new MongoDbProvider("localhost", 27017);
+
+    }
+}
