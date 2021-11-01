0ad24f12eed62d855d6a3176458bae3f267fe50d
Rudeous <46238000+Rudeous@users.noreply.github.com>
2021-11-01 19:21:08 +0800

    Revert "removed target folder"
    This reverts commit fe6e2882397ef91d535e819ea475b1acaca1d615.

--- src/main/java/scrapers/India1Scraper.java
+++ src/main/java/scrapers/India1Scraper.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD:src/main/java/scrapers/India1Scraper.java
 package scrapers;
+=======
+package demo.selenium;
+>>>>>>> Thymeleaf,-Chart.js:src/main/java/demo/selenium/Scraper.java
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.SessionNotCreatedException;
 
+<<<<<<< HEAD:src/main/java/scrapers/India1Scraper.java
+
+=======
+>>>>>>> Thymeleaf,-Chart.js:src/main/java/demo/selenium/Scraper.java
 import java.util.*;
 
 
