8f5cc9445097768aff19987a005ef905b9663c5e
md-asnawi <muhammadw.2019@sis.smu.edu.sg>
2021-11-14 18:39:38 +0800

    India Visualization enhancements

--- src/main/java/com/example/servingwebcontent/controller/IndiaController.java
+++ src/main/java/com/example/servingwebcontent/controller/IndiaController.java
@@ -1,6 +1,7 @@
 package com.example.servingwebcontent.controller;
 
 import com.example.servingwebcontent.service.IndiaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class IndiaController {
 
 	private final IndiaService indiaService;
 
+	@Autowired
 	public IndiaController(IndiaService indiaService) {
 		this.indiaService = indiaService;
 	}
